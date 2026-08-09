1e784a1be06fcb892c3eca422a1a282bec0de060
Shakey <prabesh.242049@ncit.edu.np>
2026-08-09 13:48:48 +0545

    I hate my life

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config";
 
 function AdminLogin() {
   const navigate = useNavigate();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config";
 
 function Home() {
   const navigate = useNavigate();
