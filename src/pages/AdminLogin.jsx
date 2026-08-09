b14e4bb173abe7632d3b6d6234713c2c922a8781
Shakey <prabesh.242049@ncit.edu.np>
2026-08-09 13:44:39 +0545

    Final v1

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -12,7 +12,7 @@ function AdminLogin() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/auth/login", {
+      const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/auth/login", {
+      const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
 
         headers: {
